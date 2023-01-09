95f49316b64328250c25b13f6cce495a20406737
Gustavo Lopez <gustavo@getkalto.com>
2023-01-09 01:48:07 -0300

    Shot changeof the style

--- src/views/header/index.tsx
+++ src/views/header/index.tsx
@@ -2,18 +2,21 @@ import styled from 'styled-components';
 import SubTotalOrder from '../../components/SubTotalOrder';
 
 const HeaderStyle = styled.header`
-  background: linear-gradient(45deg, red, transparent);
   display: flex;
   height: 100%;
   justify-content: space-between;
-  padding: 0 50px;
+  padding: 0 50px 10px 0;
+  border-bottom: 2px solid red;
 `;
 
 
 const LogoContainerStyle = styled.div`
     display: flex;
     justify-content: center;
-    align-items: center;
+    align-items: flex-end;
+    background: linear-gradient(0deg, red 0%, transparent);
+    padding: 0 2rem 5px 2rem;
+    border-radius: 50px;
 
     img {
       width: 200px;    
